24fae36fac524475875f57c2cea1dfbc198522d9
Agata <agata@student.edu>
2017-04-18 15:22:12 +0200

    Connection success

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(){
     
+    var showAll = document.getElementById("showall");
+    
+    showAll.onclick = function showAllGames() {
+        
+        $.ajax({
+            url:'connect.php',
+            type: 'GET',
+            data: {action: showAll},
+            processData: false,
+            contentType: false
+        })
+                .done(function(data){
+                
+                    alert(data);
+                
+                })
+                .fail(function(){
+                
+                    alert("FAIL");
+                
+}); 
+        
+    };
     
     
     
