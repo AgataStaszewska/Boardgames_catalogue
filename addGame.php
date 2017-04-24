cdcddb6948e40f3fd704173daa2cb651727062f4
Agata <agata@student.edu>
2017-04-24 20:20:22 +0200

    Z dzisiaj. Sprawdź samo addGame.php

--- addGame.php
+++ addGame.php
@@ -1,19 +1,20 @@
 <?php
 include "connect.php";
 echo "AAA";
-
-function addGame($conn){
-    echo "BBB";
     $title = $_POST['title'];
+    var_dump($title);
     $type = $_POST['type'];
-    $minPlayers = $_POST['min_players'];
-    $maxPlayers = $_POST['max_players'];
+    $minPlayers = $_POST['minPlayers'];
+    $maxPlayers = $_POST['maxPlayers'];
     $description = $_POST['description'];
+
+function addGame($title, $type, $minPlayers, $maxPlayers, $description){
+    echo "BBB";
     
     $sql = "INSERT INTO boardgames VALUES (:title, :type, :minPlayers, :maxPlayers, :description)";
     
     try {
-       $stmt = $conn->prepare($sql);
+       $stmt = $connection->prepare($sql);
        $stmt->execute([ 'title' => $title,
                         'type' => $type,
                         'minPlayers' => $minPlayers,
@@ -27,4 +28,4 @@ function addGame($conn){
        
 }
 
-echo addGame($connection);
+echo addGame($title, $type, $minPlayers, $maxPlayers, $description);
--- app.js
+++ app.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function(){
     
     var showAll = document.getElementById("showAll");
     var addGame = document.getElementById("add");
-    
+
     showAll.onclick = function showAllFromDB() {
-        
+
         $.ajax({
             
-            url:'showAllGames.php',
+            url:'showAllGames.php'
         
         })
                 .done(function(showAllGames){
@@ -23,16 +23,29 @@ document.addEventListener("DOMContentLoaded", function(){
         
     };
     
-    addGame.onclick = function addGameToDB(){
-        
+    addGame.onsubmit = function addGameToDB() {
+//    var title = document.getElementById("title").text();
+    var title = $("#title").text();
+//    var type = document.getElementById("type").text();
+    var type = $("#type").text()
+//    var minPlayers = document.getElementById("minPlayers").text();
+    var minPlayers = $("#minPlayers").text();
+//    var maxPlayers = document.getElementById("maxPlayers").text();
+    var maxPlayers = $("#maxPlayers").text();
+//    var description = document.getElementById("description").text();
+    var description = $("#description").text();
+        console.log("title");
+        console.log("AAA");
         $.ajax({
             
-            url:'addGame.php'
+            url:'addGame.php',
+            type: 'POST',
+            data: {title: title, type: type, minPlayers: minPlayers, maxPlayers: maxPlayers, description: description}
             
         })
                 .done(function(addGame){
                     
-                    addGame;
+                    
                     
         })
                 .fail(function(){
@@ -41,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function(){
                     
         });
         
-    }
+    };
     
     
     
