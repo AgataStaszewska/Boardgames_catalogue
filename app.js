5b44a20f3adf08bc8f88415bc1bad2841e259dfa
Agata <agata@student.edu>
2017-05-16 13:39:48 +0200

    Find by Title and Find by Type działają!

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", function(){
     
     var showAll = document.getElementById("showAll");
     var addGame = document.getElementById("add");
-    var findGame = document.getElementById("findGame");
+//    var findGame = document.getElementById("findGame");
+    var findByTitle = document.getElementById("findGameByTitle");
+    var findByType = document.getElementById("findGameByType");
+    var findByNumber = document.getElementById("findGameByNumber");
 
     showAll.onclick = function showAllFromDB() {
 
@@ -54,22 +57,20 @@ document.addEventListener("DOMContentLoaded", function(){
         
     };
     
-    findGame.onclick = function findGameFromDB() {
-        
-    var name = $("#name2").val();
-    var type = $("#type2").val();
-    var numberOfPlayers = $("#no_players").val();
-    console.log("AAA");
-        $.ajax({
+      findByTitle.onclick = function findGameByTitle(){
+                  
+          var name = $("#name2").val();
+          
+          $.ajax({
             
             url:'findGame.php',
             method: 'POST',
-            data: {name: name, type: type, numberOfPlayers: numberOfPlayers}      
+            data: {name: name}      
         
         })
                 .done(function(data){
                     
-                    document.getElementById("gamesFound").innerHTML = data;
+                    document.getElementById("gameTitle").innerHTML = data;
                     
                 })
                 .fail(function(){
@@ -77,8 +78,58 @@ document.addEventListener("DOMContentLoaded", function(){
                     alert("FAIL");
                 
                 }); 
+          
+      };
+      
+        findByType.onclick = function findGameByType(){
+                  
+          var type = $("#type2").val();
+          
+          $.ajax({
+            
+            url:'findGame.php',
+            method: 'POST',
+            data: {type: type}      
         
-    };
+        })
+                .done(function(data){
+                    
+                    document.getElementById("gameType").innerHTML = data;
+                    
+                })
+                .fail(function(){
+                
+                    alert("FAIL");
+                
+                }); 
+          
+      };
+    
+//    findGame.onclick = function findGameFromDB() {
+//        
+//    var name = $("#name2").val();
+//    var type = $("#type2").val();
+//    var numberOfPlayers = $("#no_players").val();
+//    console.log("AAA");
+//        $.ajax({
+//            
+//            url:'findGame.php',
+//            method: 'POST',
+//            data: {name: name, type: type, numberOfPlayers: numberOfPlayers}      
+//        
+//        })
+//                .done(function(data){
+//                    
+//                    document.getElementById("gamesFound").innerHTML = data;
+//                    
+//                })
+//                .fail(function(){
+//                
+//                    alert("FAIL");
+//                
+//                }); 
+//        
+//    };
     
     
     
--- findGame.php
+++ findGame.php
@@ -1,19 +1,61 @@
 <?php
 
 require "connect.php";
+    
+    if(isset($_POST['name'])){
+        
+         $name = $_POST['name'];
+         findGameByTitle($connection, $name);
+        
+    }
+    
+    if(isset($_POST['type'])){
+                
+       $type = $_POST['type'];
+       findGameByType($connection, $type);
+       
+    }
+    
+    if(isset($_POST['numberOfPlayers'])){
+        
+       $numberOfPlayers = $_POST['numberOfPlayers'];
+        
+    }
+    
+//    $numberOfPlayers = $_POST['numberOfPlayers'];
 
-    $name = $_POST['name'];
-    $type = $_POST['type'];
-    $numberOfPlayers = $_POST['numberOfPlayers'];
+//function findGame($connection, $name, $type, $numberOfPlayers){
+//  
+//       $sql = "SELECT * FROM boardgames WHERE name = :name OR type = :type OR (min_nop <= :numberOfPlayers AND max_nop >= :numberOfPlayers)";
+//
+//       $stmt = $connection->prepare($sql);
+//       $stmt->execute([ 'name' => $name,
+//                        'type' => $type,
+//                        'numberOfPlayers' => $numberOfPlayers
+//
+//                      ]);
+//
+//    echo "<table class='table'>";
+//            
+//    foreach($stmt->fetchAll() as $row) {
+//        
+//        echo "<tr><td>" . $row['name'] . "</td><td>" . $row['type'] . "</td><td>" . $row['description'] . "</td></tr>";
+//
+//        
+//    }
+//    
+//    echo "</table>";
+//       
+//}
+//
+//findGame($connection, $name, $type, $numberOfPlayers);
 
-function findGame($connection, $name, $type, $numberOfPlayers){
+function findGameByTitle($connection, $name){
   
-       $sql = "SELECT * FROM boardgames WHERE name = :name OR type = :type OR (min_nop <= :numberOfPlayers AND max_nop >= :numberOfPlayers)";
+       $sql = "SELECT * FROM boardgames WHERE name = :name";
 
        $stmt = $connection->prepare($sql);
        $stmt->execute([ 'name' => $name,
-                        'type' => $type,
-                        'numberOfPlayers' => $numberOfPlayers
 
                       ]);
 
@@ -21,7 +63,7 @@ function findGame($connection, $name, $type, $numberOfPlayers){
             
     foreach($stmt->fetchAll() as $row) {
         
-        echo "<tr><td>" . $row['name'] . "</td><td>" . $row['type'] . "</td><td>" . $row['description'] . "</td></tr>";
+        echo "<tr><td>" . $row['name'] . "</td><td>" . $row['type'] . "</td><td>" . $row['description'] . "</td><td>" . $row['min_nop'] . "</td><td>" . $row['max_nop'] . "</td></tr>";
 
         
     }
@@ -30,5 +72,26 @@ function findGame($connection, $name, $type, $numberOfPlayers){
        
 }
 
-findGame($connection, $name, $type, $numberOfPlayers);
+function findGameByType($connection, $type){
+  
+       $sql = "SELECT * FROM boardgames WHERE type = :type";
 
+       $stmt = $connection->prepare($sql);
+       $stmt->execute([ 'type' => $type,
+
+                      ]);
+
+    echo "<table class='table'>";
+            
+    foreach($stmt->fetchAll() as $row) {
+        
+        echo "<tr><td>" . $row['name'] . "</td><td>" . $row['type'] . "</td><td>" . $row['description'] . "</td><td>" . $row['min_nop'] . "</td><td>" . $row['max_nop'] . "</td></tr>";
+
+        
+    }
+    
+    echo "</table>";
+       
+}
+
+//findGameByTitle($connection, $name);
